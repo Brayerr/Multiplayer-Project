590ac8c5d51d7bf53db10cd58b99eb665d52f99a
TheWazzzzzup <amitmoryosef@hotmail.com>
2023-07-28 17:48:34 +0200

    שניצל

--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class PlayerCam : MonoBehaviour
 {
+    [SerializeField] GameObject playerPrefab;
+
     [SerializeField]float sensX;
     [SerializeField]float sensY;
 
@@ -16,10 +19,18 @@ public class PlayerCam : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        GameObject go = PhotonNetwork.Instantiate("playerPrefab",Vector3.zero,transform.rotation);
+        if (go.TryGetComponent<PlayerController>(out PlayerController control))
+        {
+            orientation = control.orientation;
+        }
     }
 
     private void Update()
     {
+        transform.position = orientation.position;
+
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
