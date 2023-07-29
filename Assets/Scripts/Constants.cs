e56c8171f5a14b82466cc64c938fdadf02de578c
Brayerr <brayer_alon@walla.com>
2023-07-29 22:35:03 +0300

    small fixes

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -4,19 +4,16 @@ using UnityEngine;
 
 public static class Constants
 {
-    public static string PLAYER_CHARACTER_ID_PROPERTY_KEY = "PlayerCharacterID";
-    public static string PLAYER_READY_PROPERTY_KEY = "PlayerReady";
-    public static string ROOM_EVERYONE_READY_KEY = "RoomEveryoneReady";
-    public static string PLAYER_TAG = "Player";
-    public static string ARROW_TAG = "Arrow";
-    public static string END_GAME_RPC = "EndGameRPC";
+    public const string PLAYER_TAG = "Player";
+    public const string ARROW_TAG = "Arrow";
+    public const string END_GAME_RPC = "EndGameRPC";
     public const string PLAYER_CHARACTER_ID_PROPERTY_KEY = "PlayerCharacterID";
     public const string PLAYER_READY_PROPERTY_KEY = "PlayerReady";
     public const string ROOM_EVERYONE_READY_KEY = "RoomEveryoneReady";
     public const string PLAYER_INITIALIZED_KEY = "PlayerInitialized";
-
+           
     public const string USER_UNIQUE_ID = "UserUniqueID";
-
+           
     public const string PLAYER_PREFAB_NAME = "PlayerCapsule";
     public const string SCORE_KEY_NAME = "Score";
     public const string LOAD_GAME_NAME = "JoinOrStartGame";
