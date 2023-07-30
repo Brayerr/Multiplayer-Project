c7c9681bdfaeb09e29971d28ee54c7dce8341dec
TheWazzzzzup <amitmoryosef@hotmail.com>
2023-07-30 14:39:23 +0200

    Prefab Name Change

--- Assets/Scripts/OnlineGameManager.cs
+++ Assets/Scripts/OnlineGameManager.cs
@@ -27,7 +27,7 @@ public class OnlineGameManager : MonoBehaviourPunCallbacks
     {
         if(PhotonNetwork.IsConnectedAndReady)
         {
-            GameObject go = PhotonNetwork.Instantiate($"PlayerPrefabs/playerPrefab {PhotonNetwork.LocalPlayer.CustomProperties[Constants.PLAYER_CHARACTER_ID_PROPERTY_KEY]}", new Vector3(0, 3, -8), transform.rotation);
+            GameObject go = PhotonNetwork.Instantiate($"PlayerPrefabs/playerPrefab{PhotonNetwork.LocalPlayer.CustomProperties[Constants.PLAYER_CHARACTER_ID_PROPERTY_KEY]}", new Vector3(0, 3, -8), transform.rotation);
             
             localPlayerCam.SetOrientation(localPlayerController.orientation);
 
