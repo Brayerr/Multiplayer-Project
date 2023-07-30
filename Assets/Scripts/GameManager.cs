d7014103c67207923987056f16161a333164c947
Brayerr <brayer_alon@walla.com>
2023-07-30 14:39:37 +0300

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,59 +7,59 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviourPun
 {
-    public List<int> activePlayers = new List<int>();
+    //public List<int> activePlayers = new List<int>();
 
-    //private void Start()
+    ////private void Start()
+    ////{
+    ////    AddPlayers();
+    ////}
+
+    ////public void AddPlayers()
+    ////{
+    ////    int iterator = 0;
+    ////    foreach (var item in PhotonNetwork.PlayerList)
+    ////    {
+    ////        activePlayers.Add(iterator);
+    ////        iterator++;
+    ////    }
+    ////}
+
+    //[PunRPC]
+    //public void AddPlayer(int ID)
     //{
-    //    AddPlayers();
+    //    if (PhotonNetwork.IsMasterClient) activePlayers.Add(ID);
     //}
 
-    //public void AddPlayers()
+    //[PunRPC]
+    //public void RemovePlayer(PhotonMessageInfo info)
     //{
-    //    int iterator = 0;
-    //    foreach (var item in PhotonNetwork.PlayerList)
+    //    if (PhotonNetwork.IsMasterClient)
     //    {
-    //        activePlayers.Add(iterator);
-    //        iterator++;
+    //        //foreach (var item in activePlayers)
+    //        //{
+    //        //    if (item == Id) activePlayers.Remove(item);
+    //        //}
+    //        //activePlayers.Remove(activePlayers.Count);
+    //        activePlayers.Remove(info.Sender.ActorNumber);
+
+    //        if (activePlayers.Count <= 1) EndGameLoop();
     //    }
     //}
 
-    [PunRPC]
-    public void AddPlayer(int ID)
-    {
-        if (PhotonNetwork.IsMasterClient) activePlayers.Add(ID);
-    }
-
-    [PunRPC]
-    public void RemovePlayer(PhotonMessageInfo info)
-    {
-        if (PhotonNetwork.IsMasterClient)
-        {
-            //foreach (var item in activePlayers)
-            //{
-            //    if (item == Id) activePlayers.Remove(item);
-            //}
-            //activePlayers.Remove(activePlayers.Count);
-            activePlayers.Remove(info.Sender.ActorNumber);
-
-            if (activePlayers.Count <= 1) EndGameLoop();
-        }
-    }
-
-    public void EndGameLoop()
-    {
-        if (PhotonNetwork.IsMasterClient) photonView.RPC(Constants.END_GAME_RPC, RpcTarget.AllViaServer);
-    }
+    //public void EndGameLoop()
+    //{
+    //    if (PhotonNetwork.IsMasterClient) photonView.RPC(Constants.END_GAME_RPC, RpcTarget.AllViaServer);
+    //}
 
 
-    [PunRPC]
-    public void EndGameRPC()
-    {
-        print("restarting");
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
-        PhotonNetwork.LeaveRoom();
-        PhotonNetwork.RemovePlayerCustomProperties(Constants.ProprtiesToClearOnLeaveRoom);
-        SceneManager.LoadScene(0);
-    }
+    //[PunRPC]
+    //public void EndGameRPC()
+    //{
+    //    print("restarting");
+    //    Cursor.lockState = CursorLockMode.None;
+    //    Cursor.visible = true;
+    //    PhotonNetwork.LeaveRoom();
+    //    PhotonNetwork.RemovePlayerCustomProperties(Constants.ProprtiesToClearOnLeaveRoom);
+    //    SceneManager.LoadScene(0);
+    //}
 }
